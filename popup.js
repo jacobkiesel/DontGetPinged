f4ed8f1fa680f48d1aebdb61b7882d045310caa1
jacobkiesel <jacobpkiesel@gmail.com>
2022-11-17 00:18:32 -0600

    Got an output of tabs that are making sound, tomorrow we figure out how to mute them

--- popup.js
+++ popup.js
@@ -1,16 +1,43 @@
+// chrome.tabs.onActivated.addListener(moveToFirstPosition);
 
+// async function moveToFirstPosition(activeInfo) {
+//   try {
+//     await chrome.tabs.move(activeInfo.tabId, {index: 0});
+//     console.log('Success.');
+//   } catch (error) {
+//     if (error == 'Error: Tabs cannot be edited right now (user may be dragging a tab).') {
+//       setTimeout(() => moveToFirstPosition(activeInfo), 50);
+//     } else {
+//       console.error(error);
+//     }
+//   }
+// }
 
-chrome.tabs.onActivated.addListener(moveToFirstPosition);
+const intervalID = setInterval(run, 50)
 
-async function moveToFirstPosition(activeInfo) {
-  try {
-    await chrome.tabs.move(activeInfo.tabId, {index: 0});
-    console.log('Success.');
-  } catch (error) {
-    if (error == 'Error: Tabs cannot be edited right now (user may be dragging a tab).') {
-      setTimeout(() => moveToFirstPosition(activeInfo), 50);
-    } else {
-      console.error(error);
-    }
-  }
+let queryOptions = { audible: true };
+async function talkingTabs() {
+  let [tab] = await chrome.tabs.query(queryOptions);
+  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (tab) {
+        resolve(tab);
+      } else {
+        reject();
+      }
+    }, 1);
+  });
 }
+
+
+function run() {
+  talkingTabs()
+  .then((tab) => {
+    //TODO Get me the ID of the tab and mute the tab
+    console.log(tab);
+  })
+  .catch((e) => {
+    console.log();
+  });
+};
