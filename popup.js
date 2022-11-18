2c4e1775d8768e3ae2964f4ad65071eb8e54465b
Jacob Kiesel <85920526+jacobkiesel@users.noreply.github.com>
2022-11-17 21:39:37 -0600

    Got it to mute based on tab id

--- popup.js
+++ popup.js
@@ -13,12 +13,21 @@
 //   }
 // }
 
-const intervalID = setInterval(run, 50)
+// TODO Find me a way to exclude sites from being asked at all
+
+// If (this site is whitelisted)
+// then
+//  do nothing
+// else
+//   start the timer here
+const intervalID = setInterval(run, 50);
+
+// TODO Find a way to unmute the page after its been muted AND stop the process from running on this tab again.
 
 let queryOptions = { audible: true };
 async function talkingTabs() {
   let [tab] = await chrome.tabs.query(queryOptions);
-  
+
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (tab) {
@@ -29,15 +38,19 @@ async function talkingTabs() {
     }, 1);
   });
 }
-
-
+// function to extract id and mute tab
+async function getIdAndMute(tab) {
+  console.log(tab);
+  await chrome.tabs.update(tab.id, { muted: true }); // TODO this really needs to loop over an array as of right now it only works if one tab is audible
+}
+// basically the init function, it asks the program to start listening to tabs and then handles th response
 function run() {
   talkingTabs()
-  .then((tab) => {
-    //TODO Get me the ID of the tab and mute the tab
-    console.log(tab);
-  })
-  .catch((e) => {
-    console.log();
-  });
-};
+    .then((tab) => {
+      // Here we call the function that takes the object data of the tabs that are audible and mutes them.
+      getIdAndMute(tab);
+    })
+    .catch((e) => {
+      console.log();
+    });
+}
